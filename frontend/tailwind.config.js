4503d9cd93daf09ef2230c0a300f179cb6eddfbc
Mekot <mekotsarder+heroku@gmail.com>
2024-10-06 17:47:43 -0400

    color schemes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#7E60BF',     // Deep Purple
+        secondary: '#E4B1F0',   // Light Purple/Pink
+        accent: '#FFE1FF',      // Very Light Pink
+        background: '#FAF7F0',  // Light Beige/Off-White
+      },
+    },
+  },
+  variants: {
+    extend: {
+      borderColor: ['focus'],
+      ringColor: ['focus'],
+    },
   },
   plugins: [],
 }
-
