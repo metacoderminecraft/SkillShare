59f28c19baccf31b7f89fc33bca9f1afe5bdb58e
Mekot <mekotsarder+heroku@gmail.com>
2024-10-06 18:43:02 -0400

    dashboard home signup

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,237 +1,243 @@
-import React, { useCallback } from 'react'
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ThreeDots } from 'react-loading-icons';
+import { MdCheck, MdClose } from 'react-icons/md';
+import useRedirect from '../hooks/RedirectToLogin';
 import { useUser } from '../components/UserContext';
-import Sidebar from '../components/Sidebar';
-import Modal from '../components/Modal';
-import { MdCheck, MdClose, MdChat, MdSend } from 'react-icons/md'
 
 const Dashboard = () => {
-    const [loading, setLoading] = useState(false);
-    const [incoming, setIncoming] = useState([])
-    const [currIncoming, setCurrIncoming] = useState({})
-    const [showIncoming, setShowIncoming] = useState(false)
-    const [approved, setApproved] = useState([])
-    const [currApproved, setCurrApproved] = useState({});
-    const [showApproved, setShowApproved] = useState(false);
-    const [currChat, setCurrChat] = useState({});
-    const [showChat, setShowChat] = useState(false);
-    const [currMessage, setCurrMessage] = useState('');
-    const { user } = useUser();
+  const [loading, setLoading] = useState(false);
+  const [outgoingRequests, setOutgoingRequests] = useState([]);
+  const [incomingRequests, setIncomingRequests] = useState([]);
+  const [approvedRequests, setApprovedRequests] = useState([]);
+  const { user } = useUser();
 
-    useEffect(() => {
-        const fetchIncoming = async () => {
-            setLoading(true)         
-            const response = await axios.get("http://localhost:1155/matches/incoming", { withCredentials: true });
-            setIncoming(response.data.incoming);
-            setLoading(false)
-        }
-
-        fetchIncoming();
-    }, [])
-
-    const renderIncomingDisplay = useCallback(() => {
-        return (
-            <div className='flex flex-col items-start'>
-            {incoming.map((request) => (
-                <button key={request._id} onClick={() => {
-                    setCurrIncoming(request);
-                    setShowIncoming(true);
-                }}>{request.requester.title}</button>
-            ))}
-            </div>
-        );
-    }, [incoming])
-
-    const renderRequest = useCallback(() => {
-        const render = () => (
-            <div>
-                <h1 className='font-semibold'>Offered Skill: {currIncoming.requester.title}</h1>
-                <h2>requester: {currIncoming.requester.user.username}</h2>
-                <p>focus: {currIncoming.requester.focus}</p>
-                <p>description: {currIncoming.requester.description}</p>
-                <p className='font-semibold'>Requested Skill: {currIncoming.recipient.title}</p>
-                <br />
-                <div className='flex justify-between'>
-                    <MdCheck onClick={handleApprove} className='text-green-700 cursor-pointer'></MdCheck>
-                    <MdClose onClick={handleReject} className='text-red-600 cursor-pointer'></MdClose>
-                </div>
-            </div>
-
-        )
-
-        return (
-            <Modal render={render} onClose={() => setShowIncoming(false)} />
-        )
-    }, [currIncoming, showIncoming])
-
-    const handleReject = async () => {
-        setLoading(true)
-        try {
-            await axios.post("http://localhost:1155/matches/reject", { matchId: currIncoming._id }, { withCredentials: true })
-            window.location.reload();
-        } catch (error) {
-            console.log(error);
-            alert("failure");
-        }
-        setLoading(false)
+  async function fetchData() {
+    if (!user) {
+      return;
     }
 
-    const handleApprove = async () => {
-        setLoading(true)
-        try {
-            await axios.post("http://localhost:1155/matches/approve", { matchId: currIncoming._id }, { withCredentials: true })
-            window.location.reload();
-        } catch (error) {
-            console.log(error);
-            alert("failure");
-        }
-        setLoading(false)
+    setLoading(true);
+    try {
+      const response = await axios.get('http://localhost:1155/matches/myMatches', {
+        withCredentials: true,
+      });
+      setOutgoingRequests(
+        response.data.outgoing.map((match) => ({
+          ...match,
+          dateObj: new Date(match.date),
+        }))
+      );
+      setIncomingRequests(
+        response.data.incoming.map((match) => ({
+          ...match,
+          dateObj: new Date(match.date),
+        }))
+      );
+      setApprovedRequests(
+        response.data.approved.map((match) => ({
+          ...match,
+          dateObj: new Date(match.date),
+        }))
+      );
+      response.data.rejected.forEach((match) => {
+        alert(`${match.recipient.username} rejected your match request`);
+      });
+    } catch (error) {
+      console.log(error);
     }
+    setLoading(false);
+  }
 
-    useEffect(() => {
-        const fetchApproved = async () => {
-            setLoading(true)
-            const response = await axios.get('http://localhost:1155/matches/approved', { withCredentials: true });
-            setApproved(response.data.approved);
-            setLoading(false)
-        }
+  useEffect(() => {
+    fetchData();
+  }, [user]);
 
-        fetchApproved();
-    }, [])
-
-    const renderApprovedDisplay = useCallback(() => {
-        return (
-            <div className='flex flex-col items-start'>
-            {approved.map((request) => (
-                <button key={request._id} onClick={() => {
-                    setCurrApproved(request);
-                    setShowApproved(true);
-                }}>{request.requester.title}</button>
-            ))}
-            </div>
-        );
-    }, [approved]);
-
-    const renderApproved = useCallback(() => {
-        const render = () => (
-            <div>
-                <h1 className='font-semibold'>Offered Skill: {currApproved.requester.title}</h1>
-                <h2>requester: {currApproved.requester.user.username}</h2>
-                <p>focus: {currApproved.requester.focus}</p>
-                <p>description: {currApproved.requester.description}</p>
-                <p className='font-semibold'>Requested Skill: {currApproved.recipient.title}</p>
-                <br />
-                <div className='flex justify-center items-center'>
-                    <MdChat onClick={() => {
-                        setShowApproved(false);
-                        setShowIncoming(false);
-                        setShowChat(true);
-                    }} className='cursor-pointer' />
-                </div>
-            </div>
-
-        )
-
-        return (
-            <Modal render={render} onClose={() => setShowApproved(false)} />
-        )
-    }, [currApproved, setCurrApproved])
-
-    useEffect(() => {
-        if (!showChat) {
-            return;
-        }
-
-        const fetchChat = async () => {
-            setLoading(true)
-            const recieverId = currApproved.requester.user._id == user._id ? currApproved.recipient.user._id : currApproved.requester.user._id;
-            try {
-                const response = await axios.post("http://localhost:1155/chatting/confirm", { recieverId, matchId: currApproved._id }, { withCredentials: true })
-                setCurrChat(response.data.chat);
-            } catch (error) {
-                console.log(error);
-                alert('error');
-            }
-            setLoading(false)
-        }
-
-        fetchChat();
-    }, [showChat])
-
-    const renderChat = useCallback(() => {
-        if (Object.keys(currChat) == 0) {
-            return;
-        }
-
-        const render = () => (
-            <div>
-                {currChat.messages.map((message) => (
-                    <div key={message._id} className={message.sender == user.username ? 'text-end' : 'text-start'}>
-                        <div className='m-4'>
-                            <h1 className='font-semibold'>{message.sender}</h1>
-                            <p>{message.message}</p>
-                        </div>
-                    </div>
-                    ))}
-                <div className='flex items-center justify-between'>
-                    <input
-                        type="text"
-                        placeholder="Type your message here"
-                        value={currMessage}
-                        onChange={(e) => setCurrMessage(e.target.value)}
-                        className="border-2 border-black rounded-md my-2 p-1 w-full"
-                    />
-                    <MdSend onClick={handleSend}  className='text-2xl ml-2 cursor-pointer' />
-                </div>
-            </div>  
-        )
-
-        return (
-            <Modal render={render} onClose={() => {
-                setShowChat(false);
-                setShowApproved(true);
-                setCurrMessage('');
-            }} />
-        )
-    }, [showChat, currChat, currMessage])
-
-    const handleSend = async () => {
-        if (!currMessage || !currChat) {
-            return;
-        }
-        
-        setLoading(true);
-        try {
-            const response = await axios.post('http://localhost:1155/chatting/send', { message: currMessage, chatId: currChat._id }, { withCredentials: true });
-            setCurrChat(response.data.chat);
-            setCurrMessage('');
-        } catch (error) {
-            console.log(error);
-            alert('error');
-        }
-        setLoading(false);
+  const handleApprove = async (matchId) => {
+    try {
+      await axios.post(
+        'http://localhost:1155/matches/approve',
+        { matchId },
+        { withCredentials: true }
+      );
+      fetchData(); // Refresh data without reloading the page
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-        <div>
-            {
-                loading || !user ? (
-                    <div className='flex justify-center my-20'>
-                        <ThreeDots fill="#000000" />
-                    </div>
-                ) : (
-                    <div className='flex justify-between'>
-                        <Sidebar title="Incoming" renderDisplay={renderIncomingDisplay} />
-                        {showIncoming && renderRequest()}
-                        <Sidebar title="Approved" renderDisplay={renderApprovedDisplay} />
-                        {showApproved && renderApproved()}
-                        {showChat && renderChat()}
-                    </div>
-                )
-            }
+  const handleReject = async (matchId) => {
+    try {
+      await axios.post(
+        'http://localhost:1155/matches/reject',
+        { matchId },
+        { withCredentials: true }
+      );
+      fetchData(); // Refresh data without reloading the page
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className='min-h-screen bg-background p-4'>
+      {loading || !user ? (
+        <div className='flex justify-center my-20'>
+          <ThreeDots fill='#7E60BF' />
         </div>
-    )
-}
-export default Dashboard
+      ) : (
+        <div className='max-w-5xl mx-auto'>
+          <h1 className='text-3xl font-semibold text-gray-800 text-center mb-8'>
+            Dashboard
+          </h1>
+
+          {/* Outgoing and Incoming Requests */}
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+            {/* Outgoing Requests */}
+            <div>
+              <h2 className='text-2xl font-semibold text-gray-800 mb-4 text-center'>
+                Outgoing Requests
+              </h2>
+              <div className='bg-white shadow-md rounded-lg overflow-hidden'>
+                <table className='min-w-full'>
+                  <thead className='bg-primary text-white'>
+                    <tr>
+                      <th className='px-4 py-2 text-left'>#</th>
+                      <th className='px-4 py-2 text-left'>Appointment Time</th>
+                      <th className='px-4 py-2 text-left'>Contact</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {outgoingRequests.length > 0 ? (
+                      outgoingRequests.map((match, index) => (
+                        <tr key={match._id} className='border-b'>
+                          <td className='px-4 py-2'>{index + 1}</td>
+                          <td className='px-4 py-2'>
+                            {`${match.dateObj.toLocaleDateString()} at ${match.dateObj.toLocaleTimeString([], {
+                              hour: '2-digit',
+                              minute: '2-digit',
+                            })}`}
+                          </td>
+                          <td className='px-4 py-2'>{match.recipient.username}</td>
+                        </tr>
+                      ))
+                    ) : (
+                      <tr>
+                        <td className='px-4 py-2' colSpan='3'>
+                          No outgoing requests.
+                        </td>
+                      </tr>
+                    )}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+
+            {/* Incoming Requests */}
+            <div>
+              <h2 className='text-2xl font-semibold text-gray-800 mb-4 text-center'>
+                Incoming Requests
+              </h2>
+              <div className='bg-white shadow-md rounded-lg overflow-hidden'>
+                <table className='min-w-full'>
+                  <thead className='bg-primary text-white'>
+                    <tr>
+                      <th className='px-4 py-2 text-left'>#</th>
+                      <th className='px-4 py-2 text-left'>Appointment Time</th>
+                      <th className='px-4 py-2 text-left'>Contact</th>
+                      <th className='px-4 py-2 text-center'>Options</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {incomingRequests.length > 0 ? (
+                      incomingRequests.map((match, index) => (
+                        <tr key={match._id} className='border-b'>
+                          <td className='px-4 py-2'>{index + 1}</td>
+                          <td className='px-4 py-2'>
+                            {`${match.dateObj.toLocaleDateString()} at ${match.dateObj.toLocaleTimeString([], {
+                              hour: '2-digit',
+                              minute: '2-digit',
+                            })}`}
+                          </td>
+                          <td className='px-4 py-2'>{match.requester.username}</td>
+                          <td className='px-4 py-2 flex justify-center space-x-4'>
+                            <button
+                              className='text-green-500 hover:text-green-600 transition duration-200'
+                              onClick={() => handleApprove(match._id)}
+                              aria-label='Approve'
+                            >
+                              <MdCheck size={24} />
+                            </button>
+                            <button
+                              className='text-red-500 hover:text-red-600 transition duration-200'
+                              onClick={() => handleReject(match._id)}
+                              aria-label='Reject'
+                            >
+                              <MdClose size={24} />
+                            </button>
+                          </td>
+                        </tr>
+                      ))
+                    ) : (
+                      <tr>
+                        <td className='px-4 py-2' colSpan='4'>
+                          No incoming requests.
+                        </td>
+                      </tr>
+                    )}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+
+          {/* Approved Requests */}
+          <div className='mt-12'>
+            <h2 className='text-2xl font-semibold text-gray-800 mb-4 text-center'>
+              Approved Requests
+            </h2>
+            <div className='bg-white shadow-md rounded-lg overflow-hidden'>
+              <table className='min-w-full'>
+                <thead className='bg-primary text-white'>
+                  <tr>
+                    <th className='px-4 py-2 text-left'>#</th>
+                    <th className='px-4 py-2 text-left'>Appointment Time</th>
+                    <th className='px-4 py-2 text-left'>Contact</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {approvedRequests.length > 0 ? (
+                    approvedRequests.map((match, index) => (
+                      <tr key={match._id} className='border-b'>
+                        <td className='px-4 py-2'>{index + 1}</td>
+                        <td className='px-4 py-2'>
+                          {`${match.dateObj.toLocaleDateString()} at ${match.dateObj.toLocaleTimeString([], {
+                            hour: '2-digit',
+                            minute: '2-digit',
+                          })}`}
+                        </td>
+                        <td className='px-4 py-2'>
+                          {user._id === match.recipient._id
+                            ? match.requester.username
+                            : match.recipient.username}
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td className='px-4 py-2' colSpan='3'>
+                        No approved requests.
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dashboard;
